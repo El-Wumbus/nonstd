b75406c99bb440e6f70c3c8b1818b885c072424d
Decator <decator.c@proton.me>
2025-01-16 17:56:47 -0500

    Consoladate syscall impl

--- src/asm/sys.asm
+++ src/asm/sys.asm
@@ -3,46 +3,16 @@ format ELF64
 section '.text'
 
 public sys_syscall0
-sys_syscall0:
-    mov rax, rdi
-    syscall 
-    ret
-
 public sys_syscall1
-sys_syscall1:
-    mov rax, rdi
-    mov rdi, rsi
-    syscall 
-    ret
-
 public sys_syscall2
-sys_syscall2:
-    mov rax, rdi
-    mov rdi, rsi
-    mov rsi, rdx
-    syscall
-    ret
-
 public sys_syscall3
-sys_syscall3:
-    mov rax, rdi
-    mov rdi, rsi
-    mov rsi, rdx
-    mov rdx, rcx
-    syscall
-    ret
-
 public sys_syscall4
-sys_syscall4:
-    mov rax, rdi
-    mov rdi, rsi
-    mov rsi, rdx
-    mov rdx, rcx
-    mov r10, r8
-    syscall
-    ret
-
 public sys_syscall5
+sys_syscall0:
+sys_syscall1:
+sys_syscall2:
+sys_syscall3:
+sys_syscall4:
 sys_syscall5:
     mov rax, rdi
     mov rdi, rsi
